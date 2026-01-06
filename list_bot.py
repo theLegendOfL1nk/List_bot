ca0e2130f42875e3a8de4eafef897b276ee73fb6
CubingHater <mac1110971@icloud.com>
2026-01-06 06:29:27 +0100

    Fix typo in environment variable name for BOT_TOKEN

--- list_bot.py
+++ list_bot.py
@@ -17,7 +17,7 @@ import aiohttp.web
 from collections import defaultdict
 
 # --- CONFIGURATION ---
-BOT_TOKEN = os.environ.get("BOT_TOEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 DATA_FILE = "data.json"
 STATE_FILE = "bot_state.json"
 
